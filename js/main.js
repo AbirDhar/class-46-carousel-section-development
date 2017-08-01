4a9f28d2a5a8d3ebe394378431ce8ad335d43d51
AbirDhar <rinky.raj.292@gmail.com>
2017-08-02 01:44:01 +0600

    class-46
    carousel-section-development

---
+++ js/main.js
@@ -0,0 +1,26 @@
+(function($){
+    'use strict';
+
+
+      jQuery(document).ready(function($){
+         $(".featured-trailers-carousel").owlCarousel({
+             autoplay: false,
+             loop: true,
+             items: 1,
+             nav: false,
+             dots: true,
+             
+         });
+      });
+
+
+
+      jQuery(window).load(function(){
+          
+      });
+
+
+
+
+    
+})(jQuery);
